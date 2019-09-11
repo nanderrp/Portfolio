3819309ae6a94dbc55b901535f7a22720f055476
Alexander Pierpoint <alexanderpierpoint@Rebeccas-MBP-2.hsd1.or.comcast.net>
2019-09-11 10:00:27 -0700

    Ugh

--- src/Wrapper/Big Names/BigNames.js
+++ src/Wrapper/Big Names/BigNames.js
@@ -15,39 +15,39 @@ export default class BigNames extends Component {
                 <p>Big Names</p>
                 <div className="BigNames-list">
                 <Link  className="BigNames-link" to='/Adobe' style={{textDecoration:'none'}}>
-                    <span style={{textDecoration: 'underline #101010  !important'}} className='Name'>Adobe Creative Cloud</span>
-                    <span style={{textDecoration: 'underline #101010 !important'}} className='Text'>: App Banner</span>  <img src={Lock} alt="..."className="Lock"></img>
+                    <span style={{textDecoration: 'underline   !important'}} className='Name'>Adobe Creative Cloud</span>
+                    <span style={{textDecoration: 'underline  !important'}} className='Text'>: App Banner</span>  <img src={Lock} alt="..."className="Lock"></img>
                 </Link>
                 <Link className="BigNames-link" to='/Wacom'>
-                    <span style={{textDecoration: 'underline #101010 !important'}} className='Name'>Wacom</span>
-                    <span style={{textDecoration: 'underline #101010 !important'}} className='Text'>: Product Launch Emails</span>  <img src={Lock} alt="..."className="Lock"></img>
+                    <span style={{textDecoration: 'underline  !important'}} className='Name'>Wacom</span>
+                    <span style={{textDecoration: 'underline  !important'}} className='Text'>: Product Launch Emails</span>  <img src={Lock} alt="..."className="Lock"></img>
                 </Link>
                 <Link className="BigNames-link" to='/Linkedin'>
-                    <span style={{textDecoration: 'underline #101010!important'}} className='Name'>Linkedin</span>
-                    <span style={{textDecoration: 'underline #101010 !important'}} className='Text'>: Sponsored Inmail Email</span>  <img src={Lock} alt="..."className="Lock"></img>
+                    <span style={{textDecoration: 'underline !important'}} className='Name'>Linkedin</span>
+                    <span style={{textDecoration: 'underline  !important'}} className='Text'>: Sponsored Inmail Email</span>  <img src={Lock} alt="..."className="Lock"></img>
                 </Link>
              
                 
                 <Link className="BigNames-link"  to='/UnderArmour'>
-                    <span style={{textDecoration: 'underline #101010 !important'}} className='Name'>Under Armour</span>
-                    <span style={{textDecoration: 'underline #101010 !important'}} className='Text'>: You vs the Year Emails</span>  <img src={Lock} alt="..."className="Lock"></img>
+                    <span style={{textDecoration: 'underline  !important'}} className='Name'>Under Armour</span>
+                    <span style={{textDecoration: 'underline  !important'}} className='Text'>: You vs the Year Emails</span>  <img src={Lock} alt="..."className="Lock"></img>
                 </Link>
                 <Link className="BigNames-link"  to='/Hyatt'>
-                    <span style={{textDecoration: 'underline #101010  !important'}} className='Name'>Hyatt</span>
-                    <span style={{textDecoration: 'underline #101010 !important'}} className='Text'>: Mobile Check-in</span>  <img src={Lock} alt="..."className="Lock"></img>
+                    <span style={{textDecoration: 'underline   !important'}} className='Name'>Hyatt</span>
+                    <span style={{textDecoration: 'underline  !important'}} className='Text'>: Mobile Check-in</span>  <img src={Lock} alt="..."className="Lock"></img>
                 </Link>
                 <Link className="BigNames-link"  to='/CapitalGroup'>
-                    <span style={{textDecoration: 'underline #101010 !important'}} className='Name'>Capital Group</span>
-                    <span style={{textDecoration: 'underline #101010 !important'}} className='Text'>: Email Newsletters</span>  <img src={Lock} alt="..."className="Lock"></img>
+                    <span style={{textDecoration: 'underline  !important'}} className='Name'>Capital Group</span>
+                    <span style={{textDecoration: 'underline  !important'}} className='Text'>: Email Newsletters</span>  <img src={Lock} alt="..."className="Lock"></img>
                 </Link>
               
                 <Link className="BigNames-link" to='/AARP'>
-                    <span style={{textDecoration: 'underline #101010 !important'}} className='Name'>AARP</span>
-                    <span style={{textDecoration: 'underline #101010  !important'}} className='Text'>: Girlfriend Illustration</span>  <img src={Lock} alt="..."className="Lock"></img>
+                    <span style={{textDecoration: 'underline  !important'}} className='Name'>AARP</span>
+                    <span style={{textDecoration: 'underline   !important'}} className='Text'>: Girlfriend Illustration</span>  <img src={Lock} alt="..."className="Lock"></img>
                 </Link>
                 <Link className="BigNames-link"  to='/Thesis'>
-                    <span style={{textDecoration: 'underline #101010 !important'}} className='Name'>Thesis</span>
-                    <span style={{textDecoration: 'underline #101010 !important'}} className='Text'>: Branding and Web Design</span>  <img src={Lock} alt="..."className="Lock"></img>
+                    <span style={{textDecoration: 'underline  !important'}} className='Name'>Thesis</span>
+                    <span style={{textDecoration: 'underline  !important'}} className='Text'>: Branding and Web Design</span>  <img src={Lock} alt="..."className="Lock"></img>
                 </Link>
                </div>
                 </div>
