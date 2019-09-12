a4138c0920b10b5276032577e1e680b0216b1447
Alexander Pierpoint <alexanderpierpoint@Rebeccas-MBP-2.hsd1.or.comcast.net>
2019-09-12 10:50:43 -0700

    okay

--- src/Wrapper/About/Email.js
+++ src/Wrapper/About/Email.js
@@ -4,17 +4,7 @@ import './Email.css';
 
 
 export default class Email extends Component {
-    constructor(props) {
-        super(props);
-      
-    
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
 
-    handleChange(event) {
-        this.setState({value: event.target.value});
-      }
 
     
     render() {
@@ -23,9 +13,9 @@ export default class Email extends Component {
             <div className="Email">
                 <Link className='Email-exit' to='/'> &#10005; </Link>
                 <form className="Email-form" action="https://formspree.io/beccapierpoint@gmail.com" method="POST">
-                <input className="Email-input" name="name" type="text"  placeholder="Who are you?" onChange={this.handleChange}/>
-                <input className="Email-input" name="email" type="email" placeHolder="Your Email" onChange={this.handleChange}/>
-                <textarea className="Email-body" name="message" type="text"  contentEditable="true" placeholder="Talk to me!" onChange={this.handleChange}/>
+                <input className="Email-input" name="name" type="text"  placeholder="Who are you?"/>
+                <input className="Email-input" name="email" type="email" placeHolder="Your Email"/>
+                <textarea className="Email-body" name="message" type="text"  contentEditable="true" placeholder="Talk to me!"/>
                 <input className="Email-btn" type="submit" value="Send"></input>
                 </form>
             </div>
