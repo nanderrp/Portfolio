6df3600c1eb8d75c41e058910464ce6a31bf256a
Alexander Pierpoint <alexanderpierpoint@Rebeccas-MBP-2.hsd1.or.comcast.net>
2019-09-12 10:44:30 -0700

    yay fied it

--- src/Wrapper/About/Email.js
+++ src/Wrapper/About/Email.js
@@ -15,20 +15,14 @@ export default class Email extends Component {
     handleChange(event) {
         this.setState({value: event.target.value});
       }
-    
-    handleSubmit(e){
-            e.preventDefault();
-            alert("Your Email Sent!");
-          
-            
-    }
+
     
     render() {
      
         return (
             <div className="Email">
                 <Link className='Email-exit' to='/'> &#10005; </Link>
-                <form className="Email-form" action="https://formspree.io/beccapierpoint@gmail.com" method="POST" onSubmit={this.handleSubmit}>
+                <form className="Email-form" action="https://formspree.io/beccapierpoint@gmail.com" method="POST">
                 <input className="Email-input" name="name" type="text"  placeholder="Who are you?" onChange={this.handleChange}/>
                 <input className="Email-input" name="email" type="email" placeHolder="Your Email" onChange={this.handleChange}/>
                 <textarea className="Email-body" name="message" type="text"  contentEditable="true" placeholder="Talk to me!" onChange={this.handleChange}/>
